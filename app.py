038c44ffbdd59de84125a7539a89f59517895ea6
Peduck19 <261480947+Peduck19@users.noreply.github.com>
2026-02-14 04:57:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,36 @@
 from flask import Flask, request
+import requests
+import os
 
 app = Flask(__name__)
+
+LINE_NOTIFY_TOKEN = "ใส่_tokenจริงของคุณ"
+
+def send_line(msg):
+    url = "https://notify-api.line.me/api/notify"
+    headers = {
+        "Authorization": f"Bearer {LINE_NOTIFY_TOKEN}"
+    }
+    data = {"message": msg}
+    requests.post(url, headers=headers, data=data)
 
 @app.route("/", methods=["GET"])
 def home():
     return "Gold Line Bot is running"
+
 @app.route("/webhook", methods=["POST"])
 def webhook():
     data = request.json
-    
-    symbol = data.get("symbol", "Asset")
+
     price = float(data.get("price", 0))
-    target = float(data.get("target", 0))
 
     msg = f"ราคามาถึง {price:.0f} เตรียมตัว 🚨🚨"
     send_line(msg)
     send_line(msg)
 
-    print(msg)
     print(data)
     return "OK"
-import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port))
