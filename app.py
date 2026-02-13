739971309223b9db55d32ba669e2da71001303a9
Peduck19 <261480947+Peduck19@users.noreply.github.com>
2026-02-14 05:06:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -32,6 +32,6 @@ def webhook():
     return "OK"
 import os
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
