327f4f29db74267b8ddf0e63dd6c2d554a69ffff
Peduck19 <261480947+Peduck19@users.noreply.github.com>
2026-02-14 02:19:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def home():
@@ -11,3 +11,8 @@ def webhook():
     data = request.json
     print(data)
     return "OK"
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
